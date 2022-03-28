0a2388449cc4ce413114274e1fa7cd79f58a2191
Axcle Viray <70315480+klauscle21@users.noreply.github.com>
2022-03-28 18:22:46 +0800

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -5,17 +5,17 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'db_mvcshareposts');
+define('DB_NAME', 'db_notes');
 
 
 //App Root
 define('APPROOT', dirname(__FILE__, 2));
 
 //URL Root
-define('URLROOT', 'http://localhost/MVCSharedPost');
+define('URLROOT', 'http://localhost/Notes');
 
 //Site Name
-define('SITENAME', "MVCSharedPost");
+define('SITENAME', "MyNotes");
 
 //App Version
 define('APPVERSION', "1.0.0");
