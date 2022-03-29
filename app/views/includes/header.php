f79ca9285308508816e95f9c373afebc364717cd
Axcle Viray <70315480+klauscle21@users.noreply.github.com>
2022-03-29 14:27:40 +0800

    use plugins of adminlte in footer and header

--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -7,13 +7,14 @@
     <title><?php SITENAME; ?></title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/plugins/fontawesome-free/css/all.min.css">
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Theme style -->
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/adminlte.min.css">
+    <!--  -->
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/style.css">
 </head>
-
-<?php require APPROOT . '/views/includes/navbar.php'; ?>
