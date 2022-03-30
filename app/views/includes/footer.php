ed41cf9acce598189de29faec0fa2113f8dd621a
Axcle Viray <70315480+klauscle21@users.noreply.github.com>
2022-03-30 21:12:40 +0800

    add sweet alert

--- app/views/includes/footer.php
+++ app/views/includes/footer.php
@@ -4,6 +4,5 @@
 <script src="<?php echo URLROOT; ?>/assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <script src="<?php echo URLROOT; ?>/assets/js/adminlte.min.js?v=3.2.0"></script>
 <script src="<?php echo URLROOT; ?>/assets/js/demo.js"></script>
-<script src="<?php echo URLROOT; ?>/js/script.js"></script>
-
+<script src="<?php echo URLROOT; ?>/assets/js/script.js"></script>
 </html>
--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php SITENAME; ?></title>
+    <title><?php echo SITENAME; ?></title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 
@@ -14,7 +14,11 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Theme style -->
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/adminlte.min.css">
-    <!--  -->
+    <!-- overlay scroll  -->
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <!-- sweet alert -->
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/style.css">
+
+    <link href="<?php echo URLROOT; ?>/assets/img/notes_logo.png" rel="icon">
 </head>
