3380efd4f8080227e08909d11bf69afefab9d974
Axcle Viray <70315480+klauscle21@users.noreply.github.com>
2022-03-30 11:09:08 +0800

    display notes in index

--- app/views/notes/index.php
+++ app/views/notes/index.php
@@ -1,3 +1,61 @@
 <?php require APPROOT . '/views/includes/header.php'; ?>
 <?php require APPROOT . '/views/includes/dashboard_sidebar.php'; ?>
+
+<div class="content-wrapper">
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1><?php echo $data['page_title']; ?></h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+
+                <?php if (count($data['notes']) > 0) : ?>
+                    <?php foreach ($data['notes'] as $note) : ?>
+                        <?php if ($note->user_id == $_SESSION['user_id']) : ?>
+                            <div class="col-md-3">
+                                <div class="card bg-gradient-warning">
+                                    <div class="card-header">
+                                        <b>
+                                            <?php echo $note->note_title; ?>
+                                        </b>
+                                        <div class="card-tools">
+                                            <button type="button" class="btn btn-tool"><i class="fas fa-archive"></i></button>
+                                            <button type="button" class="btn btn-tool"><i class="fas fa-times"></i></button>
+                                        </div>
+                                    </div>
+                                    <a href="<?php echo URLROOT; ?>/notes/edit/<?php echo $note->note_id; ?>" class="note-body">
+                                        <div class="card-body" style="color: black">
+                                            <?php if (strlen($note->note_body) > 50) : ?>
+                                                <?php echo substr($note->note_body, 0, 120) . '...'; ?>
+                                            <?php else : ?>
+                                                <?php echo $note->note_body; ?>
+                                            <?php endif; ?>
+                                        </div>
+                                    </a>
+                                    <div class="card-footer">
+                                        <div class="float-right">
+                                            <i><?php echo $note->noteCreatedAt; ?></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
+                <?php else : ?>
+                    <?php echo "No Notes"; ?>
+                <?php endif; ?>
+
+            </div>
+    </section>
+
+</div>
+
+<?php require APPROOT . '/views/includes/dashboard_footer.php'; ?>
 <?php require APPROOT . '/views/includes/footer.php'; ?>
