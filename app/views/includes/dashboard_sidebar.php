b5735092d72b4c0a80b3505e257afc67a888995c
Axcle Viray <70315480+klauscle21@users.noreply.github.com>
2022-03-30 21:23:52 +0800

    add website icon on sidebar

--- app/views/includes/dashboard_sidebar.php
+++ app/views/includes/dashboard_sidebar.php
@@ -15,10 +15,15 @@
         </nav>
 
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <a href="#" class="brand-link">
+                <img src="<?php echo URLROOT; ?>/assets/img/notes_logo.png" alt="MyNotes" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <span class="brand-text font-weight-light">MyNotes</span>
+            </a>
+            
             <div class="sidebar">
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="<?php echo URLROOT;?>/assets/uploads/male-avatar03.png" class="img-circle elevation-2" alt="User Image">
+                        <img src="<?php echo URLROOT; ?>/assets/img/male-avatar05.png" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
                         <a href="#" class="d-block"><?php echo $_SESSION['user_fname']; ?></a>
@@ -59,7 +64,7 @@
                         </li>
                         <li class="nav-item">
                             <a href="<?php echo URLROOT; ?>/users/logout" class="nav-link">
-                            <i class="nav-icon fas fa-solid fa-right-from-bracket"></i>
+                                <i class="nav-icon fas fa-solid fa-right-from-bracket"></i>
                                 <p> Sign Out </p>
                             </a>
                         </li>
@@ -67,6 +72,3 @@
                 </nav>
             </div>
         </aside> <!-- end of aside -->
-        
-
-        
