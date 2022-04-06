3073b9e7d4d96007e2a9c34647d769b72e9edc2f
Axcle Viray <70315480+klauscle21@users.noreply.github.com>
2022-04-06 21:34:40 +0800

    add SMTP constants

--- app/config/config.php
+++ app/config/config.php
@@ -19,3 +19,11 @@ define('SITENAME', "MyNotes");
 
 //App Version
 define('APPVERSION', "1.0.0");
+
+//SMTP (PHPMailer)
+define("MAILER_HOST", "smtp.gmail.com");
+define('MAILER_EMAIL', 'sampledummyXYZ@gmail.com');
+define("MAILER_NAME", "Sample Dummy XYZ");
+define('MAILER_PASSWORD', '@SampleDummyXYZ_21');
+define('MAILER_SMTPSECURE', 'tls');
+define('MAILER_PORT', 587);
