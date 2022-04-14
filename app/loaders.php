52808b47bed7e2534f6864f7c63835ef94ae2bf0
Axcle Viray <70315480+klauscle21@users.noreply.github.com>
2022-04-14 15:06:15 +0800

    used phpdotenv in loaders

--- app/loaders.php
+++ app/loaders.php
@@ -1,4 +1,10 @@
 <?php
+require_once realpath(__DIR__ . "/vendor/autoload.php");
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 // load config
 require_once 'config/config.php';
